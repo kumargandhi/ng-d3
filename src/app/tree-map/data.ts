59662309508da5b82136c1044b8ab3e53531c6a3
Kumar Gandhi <kumargandhi30@gmail.com>
2022-12-16 15:23:40 +0530

    Tree map changes

---
+++ src/app/tree-map/data.ts
@@ -0,0 +1,1 @@
+export const TREE_MAP_DATA = [];
--- src/app/tree-map/tree-map.component.ts
+++ src/app/tree-map/tree-map.component.ts
@@ -1,15 +1,84 @@
-import { Component, OnInit } from '@angular/core';
+import {
+    Component,
+    ElementRef,
+    OnInit,
+    OnDestroy,
+    ViewChild,
+    ChangeDetectionStrategy,
+    ChangeDetectorRef,
+} from '@angular/core';
+import * as d3 from 'd3v6';
+import { TREE_MAP_DATA } from './data';
 
 @Component({
-  selector: 'app-tree-map',
-  templateUrl: './tree-map.component.html',
-  styleUrls: ['./tree-map.component.scss']
+    selector: 'app-tree-map',
+    templateUrl: './tree-map.component.html',
+    styleUrls: ['./tree-map.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TreeMapComponent implements OnInit {
+export class TreeMapComponent implements OnInit, OnDestroy {
+    private width = 700;
+    private height = 400;
+    private margin = { top: 20, right: 20, bottom: 35, left: 20 };
 
-  constructor() { }
+    private _chart = {
+        svg: null,
+        svgInner: null,
+        tooltip: null,
+    };
 
-  ngOnInit(): void {
-  }
+    data = TREE_MAP_DATA;
 
+    @ViewChild('chartContainer', { static: true }) chartContainer: ElementRef;
+
+    constructor(private _cd: ChangeDetectorRef) {}
+
+    ngOnInit() {
+        this.initializeChart();
+        this.drawChart();
+        this._cd.markForCheck();
+    }
+
+    public ngOnChanges(changes): void {
+        if (changes.hasOwnProperty('data') && this.data) {
+            console.log(this.data);
+            this.initializeChart();
+            this.drawChart();
+
+            window.addEventListener('resize', () => this.drawChart());
+        }
+    }
+
+    private initializeChart(): void {
+        // Remove elements if already exist
+        d3.select('#chartTooltip').selectAll('*').remove();
+
+        this._chart.svg = d3
+            .select('#line')
+            .select('svg')
+            .attr('height', this.height + this.margin.top);
+        this._chart.svgInner = this._chart.svg
+            .append('g')
+            .style(
+                'transform',
+                `translate(${this.margin.left}px, ${this.margin.top}px)`
+            );
+
+        this._chart.tooltip = d3
+            .select('body')
+            .append('div')
+            .attr('id', 'chartTooltip')
+            .style('display', 'none')
+            .style('opacity', 0);
+    }
+
+    private drawChart(): void {
+        this.width =
+            this.chartContainer.nativeElement.getBoundingClientRect().width;
+        this._chart.svg.attr('width', this.width);
+    }
+
+    ngOnDestroy(): void {
+        d3.selectAll('#chartTooltip').remove();
+    }
 }
